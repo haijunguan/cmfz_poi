f9c0a029cde284408bf0bc9c5bf413058d22427e
ghj <haijunguan@163.com>
2019-05-30 20:07:31 +0800

    init 2

--- src/test/java/com/baizhi/testIn.java
+++ src/test/java/com/baizhi/testIn.java
@@ -17,8 +17,8 @@ public class testIn {
     public void testIn1() throws IOException {
         HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream("F:/三阶段框架/课堂记录/13.后期项目/150-后期项目/day7.student.xls"));
         HSSFSheet sheet = workbook.getSheet("sstudent");
-
-
-
+		System.out.println("来了，老弟！");
+		System.out.println("来了，小老弟！");
+		System.out.println("来了，老小弟！");
     }
 }
